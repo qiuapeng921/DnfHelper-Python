780ab267d93c762944ee9b68472de5a86a68809a
qiuapeng <qiuapeng@vchangyi.com>
2023-02-15 17:36:46 +0800

    驱动加载

--- driver/derive.py
+++ driver/derive.py
@@ -1,8 +1,58 @@
-import win32api
 import win32service
+import winerror
 
 
-def derive():
-    process_handle = win32api.OpenProcess(2097151, False, 9128)
-    manager_handle = win32service.OpenSCManager(None, None, 0xf003f)
-    print(process_handle, manager_handle)
+class MemoryRw:
+    # SCM句柄
+    _hSCManager = int
+    # 服务句柄
+    _hService = None
+
+    def __init__(self):
+        pass
+
+    def load_driver(self, driver_path: str, service_name: str, display_name: str) -> bool:
+        result = bool
+
+        self._hSCManager = win32service.OpenSCManager(None, None, win32service.SC_MANAGER_ALL_ACCESS)
+        if self._hSCManager == 0:
+            return False
+
+        try:
+            self._hService = win32service.CreateService(self._hSCManager, service_name, display_name,
+                                                        win32service.SERVICE_ALL_ACCESS,
+                                                        win32service.SERVICE_KERNEL_DRIVER,
+                                                        win32service.SERVICE_DEMAND_START,
+                                                        win32service.SERVICE_ERROR_IGNORE,
+                                                        driver_path, None, 0, None, None, None)
+        except win32service.error as create_err:
+            # 1073 服务已经存在
+            if create_err.winerror == winerror.ERROR_SERVICE_EXISTS:
+                self._hService = win32service.OpenService(self._hSCManager, service_name,
+                                                          win32service.SC_MANAGER_ALL_ACCESS)
+            try:
+                win32service.StartService(self._hService, None)
+            except win32service.error as start_err:
+                # 1056 服务运行,1072服务标记删除
+                if start_err.winerror == winerror.ERROR_SERVICE_ALREADY_RUNNING or start_err.winerror == winerror.ERROR_SERVICE_MARKED_FOR_DELETE:
+                    result = True
+            finally:
+                win32service.CloseServiceHandle(self._hService)
+        finally:
+            win32service.CloseServiceHandle(self._hSCManager)
+
+        return result
+
+    def un_load_driver(self, service_name: str):
+        try:
+            self._hService = win32service.OpenService(self._hSCManager, service_name, win32service.SERVICE_ALL_ACCESS)
+            try:
+                status = win32service.ControlService(self._hService, win32service.SERVICE_CONTROL_STOP)
+            finally:
+                win32service.CloseServiceHandle(self._hService)
+        except win32service.error as start_err:
+            print(start_err.winerror)
+        finally:
+            win32service.CloseServiceHandle(self._hSCManager)
+
+        return status
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import keyboard
 
 from common import globle, mem
+from common.logger import logger
+from driver.derive import MemoryRw
 from game.func import FullScreen
-
-
-def main():
-    globle.process_id = 5080
-    mem.set_process_id(globle.process_id)
-
-    full_screen = FullScreen()
-    keyboard.add_hotkey('END', full_screen.switch)
-    keyboard.wait()
-
-    while 1:
-        keyboard.wait('')
-
 
 if __name__ == '__main__':
     try:
-        main()
+        path = "C:\\RanRw.sys"
+        driver = MemoryRw()
+        if not driver.load_driver(path, "RanRw", "RanRw"):
+            logger.error("驱动加载失败")
+
+        logger.info("驱动加载成功")
+        driver.un_load_driver("RanRw")
+        globle.process_id = 5080
+        mem.set_process_id(globle.process_id)
+
+        full_screen = FullScreen()
+        keyboard.add_hotkey('END', full_screen.switch)
+        keyboard.wait()
+
+        while 1:
+            keyboard.wait('')
     except Exception as err:
         print(err.args)
     except KeyboardInterrupt as err:
