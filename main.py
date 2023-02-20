30d7c3df6a83c3914afe6c48690cc72f677a96a0
qiuapeng <qiuapeng@vchangyi.com>
2023-02-20 23:38:47 +0800

    :zap: 热键

--- main.py
+++ main.py
@@ -1,13 +1,63 @@
+import ctypes.wintypes
 import sys
+import time
 
 import win32api
+import win32con
 
-from api.hotkey import Hotkey
 from common import helper, logger, globle, mem
 from driver import driver
 from game import init_empty_addr
 
-hotkey = Hotkey()
+hotkey_run = True
+
+
+def hotkey():
+    # 加载user32.dll
+    user32 = ctypes.windll.user32
+    user32.RegisterHotKey(None, 0, 0, win32con.VK_F1)
+    user32.RegisterHotKey(None, 0, 0, win32con.VK_F2)
+    user32.RegisterHotKey(None, 0, 0, win32con.VK_F3)
+    user32.RegisterHotKey(None, 0, 0, win32con.VK_F4)
+    user32.RegisterHotKey(None, 0, 0, win32con.VK_END)
+    user32.RegisterHotKey(None, 0, 0, 192)  # 波浪
+    # user32.RegisterHotKey(None, 0, win32con.MOD_CONTROL, win32con.VK_UP)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_CONTROL, win32con.VK_DOWN)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_CONTROL, win32con.VK_LEFT)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_CONTROL, win32con.VK_RIGHT)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_ALT, win32con.VK_UP)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_ALT, win32con.VK_DOWN)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_ALT, win32con.VK_LEFT)
+    # user32.RegisterHotKey(None, 0, win32con.MOD_ALT, win32con.VK_RIGHT)
+
+    # 以下为检测热键是否被按下，并在最后释放快捷键
+    msg = ctypes.wintypes.MSG()
+    while hotkey_run:
+        time.sleep(0.1)
+        if user32.GetMessageA(ctypes.byref(msg), None, 0, 0) > 0:
+            if msg.message == win32con.WM_HOTKEY:
+                if win32api.HIWORD(msg.lParam) == win32con.VK_F1:
+                    print("VK_F1")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_F2:
+                    print("VK_F2")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_F3:
+                    print("VK_F3")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_END:
+                    print("VK_END")
+                if win32api.HIWORD(msg.lParam) == 192:
+                    print("波浪")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_UP:
+                    print("上")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_DOWN:
+                    print("下")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_LEFT:
+                    print("左")
+                if win32api.HIWORD(msg.lParam) == win32con.VK_RIGHT:
+                    print("右")
+
+            user32.TranslateMessage(ctypes.byref(msg))
+            user32.DispatchMessageA(ctypes.byref(msg))
+
 
 if __name__ == '__main__':
     process_id = helper.get_process_id_by_name("DNF.exe")
@@ -26,16 +76,14 @@ if __name__ == '__main__':
         globle.process_id = process_id
         mem.set_process_id(globle.process_id)
         init_empty_addr()
-        hotkey.run()
-
+        hotkey()
     except Exception as err:
         logger.error(err.args)
         logger.info("卸载驱动{}".format(driver.un_load_driver()))
     except KeyboardInterrupt as err:
-        hotkey.stop()
-        switch = False
+        hotkey_run = False
         logger.error(err)
         pass
     finally:
-        hotkey.stop()
+        hotkey_run = False
         pass
