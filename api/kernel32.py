158a004f4d77f8720a207609825340d8541a7deb
qiuapeng <qiuapeng@vchangyi.com>
2023-02-20 13:52:03 +0800

    update

--- api/kernel32.py
+++ api/kernel32.py
@@ -2,28 +2,28 @@ import win32api
 import win32process
 
 
-def openProcess(pid: int) -> int:
+def open_process(pid: int) -> int:
     handle = win32api.OpenProcess(2097151, False, pid)
 
     return handle
 
 
-def closeProcess(handle: int) -> int:
+def close_process(handle: int) -> int:
     return win32api.CloseHandle(handle)
 
 
-def WriteByteArr(addr: int, data: bytes) -> bool:
-    handle = openProcess(1)
+def write_byte_arr(addr: int, data: bytes) -> bool:
+    handle = open_process(1)
     win32process.WriteProcessMemory(handle, addr, data, len(data))
     return True
 
 
-def ReadByteArr(addr: int, data: bytes) -> bool:
-    handle = openProcess(1)
+def read_byte_arr(addr: int, data: bytes) -> bool:
+    handle = open_process(1)
     try:
         win32process.ReadProcessMemory(handle, addr, data, len(data))
     except Exception as msg:
         print(msg)
     finally:
-        closeProcess(handle)
+        close_process(handle)
     return True
