311498d5639c5fa5f8942988d7c606373efb15e8
qiuapeng921 <1047871481@qq.com>
2023-09-18 23:27:33 +0800

    读配置

--- common/__init__.py
+++ common/__init__.py
@@ -3,4 +3,5 @@ from configparser import ConfigParser
 
 def config():
     conf = ConfigParser()
-    return conf.read('helper.ini', encoding="utf-8-sig")
+    conf.read('helper.ini', encoding="utf-8-sig")
+    return conf
