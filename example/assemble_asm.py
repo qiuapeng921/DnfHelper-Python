de8a277e71f55db05be6babc2b1b1938327a4808
qiuapeng <qiuapeng@google.com>
2023-11-08 10:04:41 +0800

    添加将汇编转换为字节包

---
+++ example/assemble_asm.py
@@ -0,0 +1,25 @@
+from keystone import *
+
+
+def assemble_asm(asm_code):
+    try:
+        ks = Ks(KS_ARCH_X86, KS_MODE_64)  # 设置目标架构为 x86_64
+        encoding, _ = ks.asm(asm_code)
+        return bytes(encoding)
+    except KsError as e:
+        print(f"Assembly failed: {e}")
+        return None
+
+
+def main():
+    asm_code = "mov rax, 0x1234567890ABCDEF"
+    bytecode = assemble_asm(asm_code)
+
+    if bytecode:
+        print("Assembly succeeded:")
+        print(bytecode)
+        print(list(bytecode))
+
+
+if __name__ == '__main__':
+    main()
