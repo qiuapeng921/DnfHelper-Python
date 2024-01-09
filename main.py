98e39a221b7adc16aa0049346d54b293aa57874d
qiuapeng <qiuapeng@vchangyi.com>
2024-01-09 17:34:29 +0800

    代码优化

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
+import time
+
+import pygetwindow
+
 from common import helper, logger, globle, file
 from game import init, mem
-from plugins.driver import init_driver
 
 if __name__ == '__main__':
     try:
         globle.cmd = "cmd"
-        # init_driver("LoveRw")
-        # logger.info("驱动加载成功", 1)
-
         model_name = "DNF.exe"
         process_id = helper.get_process_id_by_name(model_name)
         if process_id == 0:
             logger.info("等待游戏运行...", 1)
-            while 1:
-                processId = helper.get_process_id_by_name(model_name)
-                if processId != 0:
-                    break
+            while True:
+                window = pygetwindow.getWindowsWithTitle("地下城与勇士：创新世纪")
+                if window:
+                    process_id = helper.get_process_id_by_name(model_name)
+                    if process_id != 0:
+                        mem.set_process_id(process_id)
+                        print("附加游戏成功")
+                        break
+                time.sleep(0.2)
 
         # 全局刷图计次初始化
         countCfgName = "C:\\config.ini"
         confExists = file.path_exists(countCfgName)
         if not confExists:
             file.write_ini(countCfgName, "default", "count", "0")
-
-        mem.set_process_id(process_id)
 
         # 判断是否有图标
         if mem.read_int(0x140000000) != 9460301:
